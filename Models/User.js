0191f750bee02c1a834fc2b36ba0aabde1a21c18
ConnectionsLLC <111168015+ConnectionsLLC@users.noreply.github.com>
2023-02-25 15:29:04 +0000

    addeeeg

--- Models/User.js
+++ Models/User.js
@@ -14,10 +14,6 @@ const userSchema = new mongoose.Schema({
         type: String, 
         required: true, 
         unique: true, 
-    }, 
-    password: {
-        type: String, 
-        required: true,
     },
     profile: {
         type: String, 
