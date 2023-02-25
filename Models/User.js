6bff27edc22f8deeddade276e5119b08653b2a61
ConnectionsLLC <111168015+ConnectionsLLC@users.noreply.github.com>
2023-02-25 15:34:13 +0000

    addee

--- Models/User.js
+++ Models/User.js
@@ -14,6 +14,10 @@ const userSchema = new mongoose.Schema({
         type: String, 
         required: true, 
         unique: true, 
+    }, 
+    password: {
+        type: String, 
+        required: true,
     },
     profile: {
         type: String, 
@@ -37,5 +41,14 @@ const userSchema = new mongoose.Schema({
     }
 
 })
-
+userSchema.pre('save', async function (next) {
+    const user = this;
+    console.log("Just before saving before hashing  ", user.password);
+    if (!user.isModified('password')) {
+        return next();
+    }
+    user.password = await bcrypt.hash(user.password, 8);
+    console.log("Just before saving after hashing  ", user.password);
+    next();
+})
 mongoose.model("User",userSchema)
--- index.js
+++ index.js
@@ -1,54 +1,18 @@
 const express = require('express'); 
 const port = 3000; 
-const port1 = 3001;
 const app = express(); 
 const bodyParser = require('body-parser');
 require('./db')
 require('./Models/User')
-require('./Models/Message')
 require('./Models/Post')
-
-
 const authRoutes = require('./routes/authRoutes')
 const uploadMediaRoutes = require('./routes/uploadMediaRoutes')
-const messageRoutes = require('./routes/messageRoutes')
-
-const {createServer} = require('http')
-const {Server} = require('socket.io');
-const { SocketAddress } = require('net');
-const httpServer = createServer()
-const io = new Server(httpServer, {})
-
 
 app.use(bodyParser.json())
 app.use(authRoutes)
-app.use(messageRoutes)
 app.use(uploadMediaRoutes)
-
-
 app.get('/',(req,res) => { 
     res.send("Hello World");
-})
-
-io.on('connection',(socket) => {
-    console.log("USER CONNECTED - ", socket.io)
-
-    socket.on('disconnect', () => {
-    console.log("USER DISCONNECTED - ", socket.io)
-    })
-    socket.on('join_room',(data) => {
-        console.log("USER WITH ID - ",socket.id, 'JOIN ROOM - ', data.roomid)
-        socket.join(data)
-    })
-    socket.on('send_message',(data) => {
-        console.log('MESSAGE RECEIVED - ',data)
-        io.emit('receiver message',data)
-
-    })
-})
-httpServer.listen(port1, () => {
-    console.log("Socketio Server Runnning    On Port "+port)
-
 })
 
 app.listen(port, () => {
